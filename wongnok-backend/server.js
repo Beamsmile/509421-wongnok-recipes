5177d9cafdb5c182473a73d24ad2ba8c7bd39931
Thanakorn Riksasane <thanakorn.beam@gmail.com>
2025-05-30 12:39:28 +0700

    add history

--- wongnok-backend/server.js
+++ wongnok-backend/server.js
@@ -11,7 +11,7 @@ const port = 3000;
 // Middleware
 app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500'];
+    const allowedOrigins = ['http://localhost:5500'];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
